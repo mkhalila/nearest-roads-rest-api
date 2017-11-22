37f19a8ab660e499b0f1fd84ada25bf9540ca5ab
Khalil Ahmed <mkhalila@outlook.com>
2017-11-22 22:19:29 +0000

    Install and setup morgan logger

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nearestRoads = require('nearest-roads');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+if (process.env.NODE_ENV !== 'test') {
+  // use morgan to log at command line
+  app.use(morgan('dev'));
+}
+
 // To support URL-encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.json({ message: 'Welcome to Nearest Roads!' }));
+app.get('/', (req, res) => res.json({ data: 'Welcome to Nearest Roads!' }));
 
 app.get('*', (req, res) => res.status(404).json({ error: 'Not Found.' }));
 
